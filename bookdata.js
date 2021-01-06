3cb7cd2949368b6b6c40bf08754a2226be245dc6
silencerune <silencerune@gmail.com>
2021-01-06 23:20:26 +0800

    editer_list

---
+++ bookdata.js
@@ -0,0 +1,307 @@
+let editer_list = [{
+        //     pen_name: '',
+        //     command: ``,
+        //     tab: [{
+        //         name: '',
+        //         link: ''
+        //     }],
+        //     chat: ``
+        // }, {
+        pen_name: '廠長',
+        command: `
+主要畫：我開心的٩( ᐛ )و
+`,
+        tab: [{
+            name: 'IG',
+            link: 'koujouchousama'
+        }],
+        chat: `
+請多多指教`
+    }, {
+        pen_name: '寶喵/萊伊亞',
+        command: `
+插畫
+小說
+美甲、手工藝`,
+        tab: [{
+            name: 'FB',
+            link: 'https://m.facebook.com/LaiyaPaint/'
+        }],
+        chat: `
+請大家多多指教！！
+`
+    }, {
+        pen_name: '不拉',
+        command: `
+漫畫
+`,
+        tab: [{
+            name: 'FB',
+            link: 'https://www.facebook.com/boola.boola.boo/'
+        }],
+        chat: `
+偶爾會畫一些生活小事上去(emoji)`
+    }, {
+        pen_name: '文祥',
+        command: `
+漫畫\n
+分鏡\n`,
+        tab: [{
+            name: 'IG',
+            link: 'hguuy'
+        }],
+        chat: `
+也請大家多多指教
+`
+    }, {
+        pen_name: 'Q.Mo',
+        command: `
+攝影與插畫媒合，
+圖像和文字故事為主的創作`,
+        tab: [{
+            name: '作品IG或粉專名稱',
+            link: '仿仿Imitate'
+        }],
+        chat: `
+希望大家可以喜歡我的作品唷~~~`
+    }, {
+        pen_name: '毛',
+        command: `似顏繪`,
+        tab: [{
+            name: 'IG',
+            link: 'maomaoill'
+        }],
+        chat: ``
+    },
+    {
+        pen_name: '波比好朋友',
+        command: `
+像素畫`,
+        tab: [{
+            name: 'IG',
+            link: 'haopanyo_pixel'
+        }],
+        chat: `
+大家喜歡都可以來看看XD`
+    },
+
+
+    {
+        pen_name: '貓葉',
+
+        command: `
+日系原創\n
+二創\n`,
+        // titleImage: 'black',
+        tab: [{
+                name: '推特',
+                link: 'https://twitter.com/nekoha_gc/'
+            },
+            {
+                name: '粉專',
+                link: 'https://www.facebook.com/gcobs013437/'
+            },
+            {
+                name: '繪帳',
+                link: '：https://www.facebook.com/nekohagc'
+            },
+
+        ],
+        chat: `
+現在努力發展原創中，請多多指教！`
+    },
+    {
+        pen_name: '阿化',
+
+        command: `
+插畫\n
+漫畫。\n
+`,
+        // titleImage: '2',
+        tab: [{
+            name: 'IG',
+            link: 'ahua_manga_drawing'
+        }, ],
+        chat: `
+歡迎來看看`
+
+    },
+    {
+        pen_name: '海龜',
+
+        command: `
+插畫\n
+漫畫\n`,
+        // titleImage: '2',
+        tab: [{
+            name: 'IG',
+            link: '@h1_guen77'
+        }, ],
+        chat: `
+粉專：\n
+原創：那那卡咩\n
+二創：海龜湯\n
+作品類型：漫畫、插畫\n
+（多歐美/卡通/可愛/人物/食物）\n
+(´･ω･` + '`' + `)嗨！喜歡歡迎內洽合作呦！\n
+`
+
+    }, {
+        pen_name: '草苺奶油餅',
+
+        command: `
+原創插畫\n
+畫妹仔居多\n
+`,
+
+        tab: [{
+                name: '作品',
+                link: 'pannkaka1515.tumblr.com/Artworks'
+            },
+            {
+                name: '推特',
+                link: 'twitter.com/pannkaka1515'
+            }
+        ],
+        chat: `
+筆名是CYAN.但朋友叫法很多沒有固定，通稱圓圓或學姊(usagi)
+畫妹仔居多，塗鴉跟閒聊比較多在推特`
+
+    }, {
+        pen_name: '骨頭',
+
+        command: `
+原創\n
+二創\n
+`,
+        // titleImage: '2',
+        tab: [{
+            name: 'IG',
+            link: 'https://reurl.cc/exo8qm'
+        }, ],
+        chat: `
+我很無害，歡迎教朋友   `
+
+    }, {
+        pen_name: '黑翼冰嵐',
+
+        command: `
+原創漫畫\n
+動畫師\n
+插畫\n`,
+        // titleImage: '2',
+        tab: [{
+            name: '作品集網站',
+            link: 'https://blackwingbinlan.myportfolio.com/work'
+        }, {
+            name: 'FB',
+            link: 'https://www.facebook.com/blackwingbinlan'
+        }, {
+            name: 'IG',
+            link: 'https:www.instagram.com/blackwingbinlan/'
+        }, ],
+        chat: `
+ 喜歡畫黑暗漫畫和動物、帥哥，\n
+ 目前有在接台灣和日本的動畫案子，\n
+ 漫畫方面預計會自行出版單行本。\n
+
+歡迎合作洽談和交友喔!\n
+ 如果對動畫有興趣我能介紹XD動畫界很缺人~ \n `
+
+    }, {
+        pen_name: '齊二口',
+        command: `
+似顏繪\n
+原創插畫、一點攝影\n
+原創百合跟靜物場景\n`,
+        // titleImage: '2',
+        tab: [{
+            name: 'IG',
+            link: 'https://www.instagram.com/x70215/'
+        }, {
+            name: 'plurk',
+            link: 'https://www.plurk.com/x70215'
+        }, {
+            name: 'FB',
+            link: 'https://www.facebook.com/x70215/'
+        }, ],
+        chat: `
+    目前主要在畫原創百合跟靜物場景，\n
+    喜歡畫連續圖樣/花草/少女，還請多指教(*´꒳` + '`' + `*)
+`
+
+    },
+    {
+        pen_name: '卓米',
+
+        command: `
+似顏繪`,
+        // titleImage: '2',
+        tab: [{
+            name: 'IG',
+            link: 'https://www.instagram.com/drawme.drawme/'
+        }, ],
+        chat: `
+大家豪挖 請多多指教
+`
+
+    }, {
+        pen_name: '李彤、Alex',
+
+        command: `
+漫畫、插畫`,
+        // titleImage: '2',
+        tab: [{
+            name: '作品連結',
+            link: 'https://www.facebook.com/healingskyalex.com.tw/'
+        }, {
+            name: '作品連結',
+            link: 'https://www.facebook.com/lovelyfool.com.tw/'
+        }, ],
+        chat: `
+歡迎多交流，多指教
+`
+
+    }, {
+        pen_name: '玖蒼惟夜',
+
+        command: `
+原創、二創
+    `,
+        // titleImage: '2',
+        tab: [{
+            name: 'IG',
+            link: 'jiucangweiye'
+        }, {
+            name: 'FB、推特、噗浪',
+            link: '玖蒼惟夜'
+        }, ],
+        chat: `
+雖然是佛系作家，但還是希望能受到關注。
+還請大家多多指教。
+`,
+
+    },
+];
+let active_list = [{
+    date: '2020-11-01',
+    active_descript: '測試',
+    address: '台北市'
+}];
+
+/*=================================================================================== */
+var allSerise = [{
+        labelName: '創作者',
+        labelcode: 'editer',
+        chaterSame: true,
+        data: editer_list,
+
+    },
+    {
+        labelName: '活動公告',
+        labelcode: 'active',
+        data: active_list
+    },
+
+
+]
